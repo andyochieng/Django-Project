bbd8eca72c2a52f9e0ea3a44160c8853ccd7a36e
andyochieng <andyochieng18@gmail.com>
2024-11-11 13:59:44 +0300

    Django project start

---
+++ todo_project/todo/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render,redirect
+from .models import Task
+from .forms import TaskForm
+def task_list(request):
+    # all records in the db
+    # rows : represent the object /columns
+    tasks = Task.objects.all()
+    return render(request,'todo/task_list.html',{'tasks':tasks})
+
+def task_create(request):
+    # cover validity
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'todo/task_form.html',{'form': form})
